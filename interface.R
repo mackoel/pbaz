62687255309182c78c9686a94c5bc6d19ed149ee
Konstantin Kozlov <kozlov_kn@spbstu.ru>
2018-01-26 16:17:03 +0300

    Small fixes

--- interface.R
+++ interface.R
@@ -1,12 +1,10 @@
-get_gt_snp <- function(conn, ofl, gt_like = '%', gt_limit = FALSE, snp_like = '%', snp_limit = FALSE) {
+get_gt_snp <- function(conn, ofl, gt_like = '%', gt_limit = -1, snp_like = '%', snp_limit = -1, snp_quality = -1) {
     ret <- 0
     #FIXME check input for errors
-    if (gt_limit <= 0) {
-	return(1)
-    }
-    if (snp_limit <= 0) {
-	return(2)
-    }
+    gt_limit = as.numeric(gt_limit)
+    snp_limit = as.numeric(snp_limit)
+    snp_quality = as.numeric(snp_quality)
+    #FIXME sanitize input
     # do job
     SQL <- paste0("SELECT gname FROM genotype WHERE gname LIKE '", gt_like, "'")
     if (gt_limit > 0) {
@@ -15,6 +13,9 @@ get_gt_snp <- function(conn, ofl, gt_like = '%', gt_limit = FALSE, snp_like = '%
     ret.gt.df <- dbGetQuery(conn, SQL)
     #FIXME check input for errors
     SQL <- paste0("SELECT snpabbr FROM snp WHERE snpabbr LIKE '", snp_like, "'")
+    if (snp_quality > 0) {
+	SQL <- paste0(SQL, " AND snp.quality > ", snp_quality)
+    }
     if (snp_limit > 0) {
 	SQL <- paste0(SQL, " LIMIT ", snp_limit)
     }
@@ -45,7 +46,6 @@ get_gt_snp <- function(conn, ofl, gt_like = '%', gt_limit = FALSE, snp_like = '%
 		    return(ret.row)
 		}
 	    ))
-    print(ret_data)
     ret.df <- data.frame(ret_data, row.names = ret.gt.df$gname)
     colnames(ret.df) <- ret.snp.df$snpabbr
     write.csv(ret.df, file = ofl, row.names = TRUE)
